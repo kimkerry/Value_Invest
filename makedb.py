80bbff3d52e4e8464927bf994a347b2873d354de
kimkerry <financekerry@gmail.com>
2020-03-25 22:05:26 +0900

    25일

--- makedb.py
+++ makedb.py
@@ -3,6 +3,7 @@ import time
 from bs4 import BeautifulSoup
 from IPython.display import display_html
 import pandas as pd
+import re
 import sqlite3
 
 
@@ -17,22 +18,36 @@ def make_db():
     html_is = chrome.page_source
     soup_is = BeautifulSoup(html_is, 'html.parser')
     html_fs_is = soup_is.find('table', {'class': 'gHead01 all-width data-list'})
-    df_fs_is = pd.read_html(str(html_fs_is))
-    df_fs_is = df_fs_is[0]
+    table_value = []
+    head_value =[]
+    for i in html_fs_is.find_all('tr'):
+        for h in i.find_all('th'):
+            head_value.append(h.get_text())
+        value = []
+        for ii in i.find_all('td'):
+            value.append(ii.get_text())
+        table_value.append(value)
+    df_fs_is = pd.DataFrame(data = table_value, columns = head_value)
+    df_fs_is.replace(['\t','\n'],'')
+
+    # df_fs_is = df_fs_is.set_index(df_fs_is.iloc[:,0], drop=True)
+
+    # df_fs_is = df_fs_is.str.replace(pat=r'[\s\s+]', repl = r' ', regex = True)
+    # df_fs_is = df_fs_is[0]
     # todo 1. 표의 펼치기 부분은 데이터프레임으로 들어가지 않음 -> 데이타는 있음에도 불구하고 아마도 컬럼명이 하나라서 맨 앞의것만 나오는거 같음
 
-    chrome.find_element_by_xpath('//*[@id="rpt_td2"]').click()
-    time.sleep(5)
-    html_bs = chrome.page_source
-    soup_bs = BeautifulSoup(html_bs, 'html.parser')
-    fs_bs = soup_bs.find('table', {'class': 'gHead01 all-width data-list'})
+    # chrome.find_element_by_xpath('//*[@id="rpt_td2"]').click()
+    # time.sleep(5)
+    # html_bs = chrome.page_source
+    # soup_bs = BeautifulSoup(html_bs, 'html.parser')
+    # fs_bs = soup_bs.find('table', {'class': 'gHead01 all-width data-list'})
     # todo 2. 1번 todo를 완성하고 나서 여기랑 밑에도 같이 수정해야 함
 
-    chrome.find_element_by_xpath('//*[@id="rpt_td3"]').click()
-    time.sleep(5)
-    html_cf = chrome.page_source
-    soup_cf = BeautifulSoup(html_cf, 'html.parser')
-    fs_cf = soup_cf.find('table', {'class': 'gHead01 all-width data-list'})
+    # chrome.find_element_by_xpath('//*[@id="rpt_td3"]').click()
+    # time.sleep(5)
+    # html_cf = chrome.page_source
+    # soup_cf = BeautifulSoup(html_cf, 'html.parser')
+    # fs_cf = soup_cf.find('table', {'class': 'gHead01 all-width data-list'})
 
 
     chrome.close()
@@ -46,11 +61,11 @@ def make_db():
 
 
     # 여기는 테스트 부분임
-    print(df_fs_is)
-    print('______________________________________________________________________________________________________________________________________________________________________________________')
-    print(fs_bs)
-    print('______________________________________________________________________________________________________________________________________________________________________________________')
-    print(fs_cf)
+    print(df_fs_is.head(20))
+    # print('______________________________________________________________________________________________________________________________________________________________________________________')
+    # print(fs_bs)
+    # print('______________________________________________________________________________________________________________________________________________________________________________________')
+    # print(fs_cf)
 
 
 
